b2257662218eb8552d3f87cc3d66dece65fa6a70
chiaradiaz1991 <chiara.diaz1991@gmail.com>
2019-11-29 08:34:23 -0300

    added list component

--- app/index.js
+++ app/index.js
@@ -9,8 +9,9 @@ EStylesheet.build ({
     $primaryBlue: '#4F6D7A',
     $white: '#FFFFFF',
     $lightGray: '#F0F0F0',
-    $border: '#979797',
+    $border: '#E2E2E2',
     $inputText: '#797979',
+    $darkText: '#343434'
     
 })
 
