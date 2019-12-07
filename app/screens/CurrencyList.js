d86b13389d6f5556164c0647b4f4dcec3aeff639
chiaradiaz1991 <chiara.diaz1991@gmail.com>
2019-12-07 11:40:48 -0300

    added checkmark and styles

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import { View, FlatList, StatusBar } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, FlatList, StatusBar } from 'react-native';
 import currencies from '../data/currencies';
 
-import { ListItem } from '../components/List';
+import { ListItem, Separator } from '../components/List';
 
-const CurrencyList = () => (
-    <View style={{flex:1}}>
-        <StatusBar barStyle="default" translucent={false} />
-        <FlatList
-            data={currencies}
-            renderItem={({ item }) => <ListItem />}
-            keyExtractor= {item => item}
-        />
-    </View>
-)
+const TEMP_CURRENT_CURRENCY = 'CAD';
 
+class CurrencyList extends Component {
+    handlePress = () => {
+        console.log('press');
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <StatusBar translucent={false} barStyle="light-content" />
+                <FlatList
+                    data={currencies}
+                    renderItem={({ item }) => (
+                        <ListItem
+                            text={item}
+                            selected={item === TEMP_CURRENT_CURRENCY}
+                            onPress={this.handlePress}
+                        />
+                    )}
+                    keyExtractor={item => item}
+                    ItemSeparatorComponent={Separator}
+                />
+            </View>
+        )
+    }
+}
 export default CurrencyList;
